32cab7f3ef1c7fdb7e7e6e0a53e9695704202991
Garam Kim <garam.ttree@gmail.com>
2023-06-19 17:57:44 +0900

    빼먹은거 커밋

---
+++ sources/include/utils/non_copyable.hpp
@@ -0,0 +1,20 @@
+
+#pragma once
+
+#include "HAL/attributes.hpp"
+
+namespace ivd
+{
+class DLLEXPORT non_copyable
+{
+protected:
+    non_copyable()          = default;
+    virtual ~non_copyable() = default;
+
+public:
+    non_copyable(const non_copyable&)                = delete;
+    non_copyable(non_copyable&&) noexcept            = default;
+    non_copyable& operator=(const non_copyable&)     = delete;
+    non_copyable& operator=(non_copyable&&) noexcept = default;
+};
+} // namespace ivd
