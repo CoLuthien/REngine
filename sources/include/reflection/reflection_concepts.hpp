c1b1ae1ea877b2a0b1e7c82a4f6c733117bf7bb3
garam <garam.ttree@gmail.com>
2023-09-05 17:59:22 +0900

    make concept always handle with object pointer type

--- sources/include/reflection/reflection_concepts.hpp
+++ sources/include/reflection/reflection_concepts.hpp
@@ -26,16 +26,36 @@ concept is_reflected_type = requires {
 template <typename T>
 concept is_reflected_object_queue =
     meta::is_specialization<T, std::queue> &&
-    is_reflected_type<typename meta::remove_all_qualifiers_t<T>::value_type>;
+    is_reflected_type<typename meta::remove_all_qualifiers_t<T>::value_type> &&
+    std::is_pointer_v<typename meta::remove_all_qualifiers_t<T>::value_type>;
 
 template <typename T>
 concept is_reflected_object_vector =
     meta::is_specialization<T, std::vector> &&
-    is_reflected_type<typename meta::remove_all_qualifiers_t<T>::value_type>;
+    is_reflected_type<typename meta::remove_all_qualifiers_t<T>::value_type> &&
+    std::is_pointer_v<typename meta::remove_all_qualifiers_t<T>::value_type>;
 
 template <typename T>
 concept is_reflected_object_map =
     meta::is_specialization<T, std::unordered_map> &&
-    is_reflected_type<typename meta::remove_all_qualifiers_t<T>::mapped_type>;
+    is_reflected_type<typename meta::remove_all_qualifiers_t<T>::mapped_type> &&
+    std::is_pointer_v<typename meta::remove_all_qualifiers_t<T>::mapped_type>;
+
+template <typename T>
+concept is_fundamental_queue =
+    meta::is_specialization<T, std::queue> &&
+    std::is_fundamental_v<typename meta::remove_all_qualifiers_t<T>::value_type> &&
+    (std::is_pointer_v<typename meta::remove_all_qualifiers_t<T>::value_type> == false);
+
+template <typename T>
+concept is_fundamental_vector =
+    meta::is_specialization<T, std::vector> &&
+    std::is_fundamental_v<typename meta::remove_all_qualifiers_t<T>::value_type> &&
+    (std::is_pointer_v<typename meta::remove_all_qualifiers_t<T>::value_type> == false);
+template <typename T>
+concept is_fundamental_map =
+    meta::is_specialization<T, std::unordered_map> &&
+    std::is_fundamental_v<typename meta::remove_all_qualifiers_t<T>::mapped_type> &&
+    (std::is_pointer_v<typename meta::remove_all_qualifiers_t<T>::mapped_type> == false);
 
 } // namespace refl
